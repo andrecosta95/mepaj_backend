7f289563a1f22e1d064c7f74b47a480ed5885bd3
André Costa <luz_ia_costa@hotmail.com>
2020-10-21 00:00:20 -0300

    Add files via upload
    Initial

---
+++ src/main/java/com/mepaj/MepajApplication.java
@@ -0,0 +1,13 @@
+package com.mepaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MepajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MepajApplication.class, args);
+	}
+
+}
